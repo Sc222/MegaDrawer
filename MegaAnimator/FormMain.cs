d05494c2c9bf44897b978606f1bb8ff08e5216df
sc222 <v.a.shaternikov@gmail.com>
2021-03-24 02:25:59 +0500

    add new project for animations

---
+++ MegaAnimator/FormMain.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace MegaAnimator
+{
+    public partial class FormMain : Form
+    {
+        private Graphics g;
+        
+        public FormMain() => InitializeComponent();
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            AllocConsole();
+            g = panelDrawCanvas.CreateGraphics();
+        }
+        
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AllocConsole();
+        
+    }
+}
