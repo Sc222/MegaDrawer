a7a7eedfd74e54ee679b38f0d724f4e257450c12
sc222 <v.a.shaternikov@gmail.com>
2021-03-24 01:06:14 +0500

    finish drawers logic, erase and remove items using linkedList to prevent objects overlapping

--- MegaDrawer/FormMain.cs
+++ MegaDrawer/FormMain.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp2
@@ -32,135 +33,152 @@ namespace WindowsFormsApp2
         private int yRegularPolygonInput;
         private int rRegularPolygonInput;
         private int nRegularPolygonInput;
-
-        private readonly Stack<LineDrawer> lineDrawers = new Stack<LineDrawer>();
-        private readonly Stack<CircleDrawer> circleDrawers = new Stack<CircleDrawer>();
-        private readonly Stack<PolygonDrawer> polygonDrawers = new Stack<PolygonDrawer>();
-
-        private readonly Stack<RegularPolygonDrawer> regularPolygonDrawers = new Stack<RegularPolygonDrawer>();
-        //todo update all canvas method private List<IDrawer> drawers = new List<IDrawer>();
-
-        public FormMain()
-        {
-            InitializeComponent();
-        }
         
+        private readonly Dictionary<Type, Drawer> lastDrawers = Drawer.InitDrawersDictionary();
+        private readonly List<Drawer> removeCandidates = new List<Drawer>();
+        private readonly LinkedList<Drawer> drawers = new LinkedList<Drawer>();
+        
+        public FormMain() => InitializeComponent();
+
         private void FormMain_Load(object sender, EventArgs e)
         {
+            AllocConsole();
             g = panelDrawCanvas.CreateGraphics();
         }
-
+        
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AllocConsole();
+        
+        private void UpdateLastObjectDrawers(Dictionary<Type,Drawer>lastDrawersTemp)
+        {
+            foreach (var drawer in lastDrawersTemp)
+                lastDrawers[drawer.Key] = drawer.Value;
+        }
+        
+        private void RedrawAllObjects()
+        {
+            g.Clear(Color.White);
+            var lastDrawersTemp = Drawer.InitDrawersDictionary();
+            for(var drawer = drawers.First; drawer != null;)
+            {
+                var next = drawer.Next;
+                if (removeCandidates.Contains(drawer.Value)) 
+                {
+                    drawers.Remove(drawer);
+                    removeCandidates.Remove(drawer.Value);
+                }
+                else
+                {
+                    var drawerType = drawer.Value.GetType();
+                    if (lastDrawersTemp.ContainsKey(drawerType))
+                        lastDrawersTemp[drawerType] = drawer.Value;
+                    drawer.Value.Draw(g);
+                }
+                drawer = next;
+            }
+            UpdateLastObjectDrawers(lastDrawersTemp);
+        }
+        
+        private void UpdateValuesAndDrawObject(Drawer drawer)
+        {
+            lastDrawers[drawer.GetType()] = drawer;
+            drawers.AddLast(drawer);
+            drawer.Draw(g);
+        }
+        
+        private void EraseObjectIfExists(Type drawerType)
+        {
+            if (lastDrawers[drawerType] == null)
+                return;
+            removeCandidates.Add(lastDrawers[drawerType]);
+            RedrawAllObjects();
+        }
+        
         //Draw and erase methods
 
         private void buttonDrawLine_Click(object sender, EventArgs e)
         {
             var lineDrawer = new LineDrawer(x1LineInput, y1LineInput, x2LineInput, y2LineInput, widthLineInput);
-            lineDrawers.Push(lineDrawer);
-            lineDrawer.Draw(g);
+            //todo check for input errors
+            UpdateValuesAndDrawObject(lineDrawer);
         }
-
-        private void buttonEraseLine_Click(object sender, EventArgs e)
-        {
-            if (lineDrawers.Count <= 0)
-                return;
-            var lineDrawer = lineDrawers.Pop();
-            lineDrawer.Erase(g);
-            ////todo clear fields method
-        }
-
+        
+        
         private void buttonDrawCircle_Click(object sender, EventArgs e)
         {
             var circleDrawer = new CircleDrawer(xCircleInput, yCircleInput, radiusCircleInput);
-            circleDrawers.Push(circleDrawer);
-            circleDrawer.Draw(g);
+            //todo check for input errors
+            UpdateValuesAndDrawObject(circleDrawer);
         }
-
-        private void buttonEraseCircle_Click(object sender, EventArgs e)
-        {
-            if (circleDrawers.Count <= 0)
-                return;
-            var circleDrawer = circleDrawers.Pop();
-            circleDrawer.Erase(g);
-            ////todo clear fields method
-        }
-
+        
         private void buttonDrawPolygon_Click(object sender, EventArgs e)
         {
             if (points.Count <= 2) return;
             var polygonDrawer = new PolygonDrawer(points);
-            polygonDrawers.Push(polygonDrawer);
-            polygonDrawer.Draw(g);
+            //todo check for input errors
+            UpdateValuesAndDrawObject(polygonDrawer);
         }
-
-        private void buttonErasePolygon_Click(object sender, EventArgs e)
-        {
-            if (polygonDrawers.Count <= 0)
-                return;
-            var polygonDrawer = polygonDrawers.Pop();
-            polygonDrawer.Erase(g);
-
-            //todo clear fields method
-        }
-
+        
         private void buttonDrawRegularPolygon_Click(object sender, EventArgs e)
         {
             if (nRegularPolygonInput <= 2) return;
             var regularPolygonDrawer = new RegularPolygonDrawer(xRegularPolygonInput, yRegularPolygonInput,
                 rRegularPolygonInput, nRegularPolygonInput);
-            regularPolygonDrawers.Push(regularPolygonDrawer);
-            regularPolygonDrawer.Draw(g);
+            //todo check for input errors
+            UpdateValuesAndDrawObject(regularPolygonDrawer);
         }
 
-        private void buttonEraseRegularPolygon_Click(object sender, EventArgs e)
-        {
-            if (regularPolygonDrawers.Count <= 0) return;
-            var regularPolygonDrawer = regularPolygonDrawers.Pop();
-            regularPolygonDrawer.Erase(g);
-            //todo clear fields method
-        }
+        private void buttonEraseLine_Click(object sender, EventArgs e) => EraseObjectIfExists(typeof(LineDrawer));
+
+        private void buttonEraseCircle_Click(object sender, EventArgs e) => EraseObjectIfExists(typeof(CircleDrawer));
+
+        private void buttonErasePolygon_Click(object sender, EventArgs e) => EraseObjectIfExists(typeof(PolygonDrawer));
+        
+        private void buttonEraseRegularPolygon_Click(object sender, EventArgs e) => EraseObjectIfExists(typeof(RegularPolygonDrawer));
 
         //Line inputs
 
         private void textBoxX1Line_TextChanged(object sender, EventArgs e)
         {
-            x1LineInput = processInput(x1LineInput, textBoxX1.Text, labelX1, "X1");
+            x1LineInput = ProcessInput(x1LineInput, textBoxX1.Text, labelX1, "X1");
         }
 
         private void textBoxY1Line_TextChanged(object sender, EventArgs e)
         {
-            y1LineInput = processInput(y1LineInput, textBoxY1.Text, labelY1, "Y1");
+            y1LineInput = ProcessInput(y1LineInput, textBoxY1.Text, labelY1, "Y1");
         }
 
         private void textBoxX2Line_TextChanged(object sender, EventArgs e)
         {
-            x2LineInput = processInput(x2LineInput, textBoxX2.Text, labelX2, "X2");
+            x2LineInput = ProcessInput(x2LineInput, textBoxX2.Text, labelX2, "X2");
         }
 
         private void textBoxY2Line_TextChanged(object sender, EventArgs e)
         {
-            y2LineInput = processInput(y2LineInput, textBoxY2.Text, labelY2, "Y2");
+            y2LineInput = ProcessInput(y2LineInput, textBoxY2.Text, labelY2, "Y2");
         }
 
         private void textBoxWidthLine_TextChanged(object sender, EventArgs e)
         {
-            widthLineInput = processInput(widthLineInput, textBoxWidth.Text, labelWidth, "W");
+            widthLineInput = ProcessInput(widthLineInput, textBoxWidth.Text, labelWidth, "W");
         }
 
         //Circle inputs
 
         private void textBoxCircleX_TextChanged(object sender, EventArgs e)
         {
-            xCircleInput = processInput(xCircleInput, textBoxCircleX.Text, labelCircleX, "X");
+            xCircleInput = ProcessInput(xCircleInput, textBoxCircleX.Text, labelCircleX, "X");
         }
 
         private void textBoxCircleY_TextChanged(object sender, EventArgs e)
         {
-            yCircleInput = processInput(yCircleInput, textBoxCircleY.Text, labelCircleY, "Y");
+            yCircleInput = ProcessInput(yCircleInput, textBoxCircleY.Text, labelCircleY, "Y");
         }
 
         private void textBoxCircleRadius_TextChanged(object sender, EventArgs e)
         {
-            radiusCircleInput = processInput(radiusCircleInput, textBoxCircleRadius.Text, labelCircleRadius, "R");
+            radiusCircleInput = ProcessInput(radiusCircleInput, textBoxCircleRadius.Text, labelCircleRadius, "R");
         }
 
         //Polygon inputs
@@ -183,37 +201,35 @@ namespace WindowsFormsApp2
             if (listBoxSelectedPoint < 0 || listBoxSelectedPoint > points.Count) return;
             points.RemoveAt(listBoxSelectedPoint);
             listBoxPolygon.Items.RemoveAt(listBoxSelectedPoint);
-            //listBoxPolygon.Items.Add()
         }
 
         private void textBoxPolygonY_TextChanged(object sender, EventArgs e)
         {
-            yPolygonPointInput = processInput(yPolygonPointInput, textBoxPolygonY.Text, labelPolygonY, "Y");
+            yPolygonPointInput = ProcessInput(yPolygonPointInput, textBoxPolygonY.Text, labelPolygonY, "Y");
         }
 
         private void textBoxPolygonX_TextChanged(object sender, EventArgs e)
         {
-            xPolygonPointInput = processInput(xPolygonPointInput, textBoxPolygonX.Text, labelPolygonX, "X");
+            xPolygonPointInput = ProcessInput(xPolygonPointInput, textBoxPolygonX.Text, labelPolygonX, "X");
         }
-
-        private static int processInput(int value, string input, Label label, string name)
+        
+        private static int ProcessInput(int value, string input, Control label, string name)
         {
             int result = value;
             if (input == "")
             {
-                label.Text = "Введите " + name;
+                label.Text = $"Введите {name}";
                 return result;
             }
 
-            int tmp;
-            bool isSuccess = int.TryParse(input, out tmp);
+            var isSuccess = int.TryParse(input, out var tmp);
             if (isSuccess)
             {
                 result = tmp;
                 label.Text = name;
             }
             else
-                label.Text = name + " Ошибка";
+                label.Text = $"{name} Ошибка";
 
             return result;
         }
@@ -223,25 +239,25 @@ namespace WindowsFormsApp2
         private void textBoxRegularPolygonN_TextChanged(object sender, EventArgs e)
         {
             nRegularPolygonInput =
-                processInput(nRegularPolygonInput, textBoxRegularPolygonN.Text, labelRegularPolygonN, "N");
+                ProcessInput(nRegularPolygonInput, textBoxRegularPolygonN.Text, labelRegularPolygonN, "N");
         }
 
         private void textBoxRegularPolygonR_TextChanged(object sender, EventArgs e)
         {
             rRegularPolygonInput =
-                processInput(rRegularPolygonInput, textBoxRegularPolygonR.Text, labelRegularPolygonR, "R");
+                ProcessInput(rRegularPolygonInput, textBoxRegularPolygonR.Text, labelRegularPolygonR, "R");
         }
 
         private void textBoxRegularPolygonY_TextChanged(object sender, EventArgs e)
         {
             yRegularPolygonInput =
-                processInput(yRegularPolygonInput, textBoxRegularPolygonY.Text, labelRegularPolygonY, "Y");
+                ProcessInput(yRegularPolygonInput, textBoxRegularPolygonY.Text, labelRegularPolygonY, "Y");
         }
 
         private void textBoxRegularPolygonX_TextChanged(object sender, EventArgs e)
         {
             xRegularPolygonInput =
-                processInput(xRegularPolygonInput, textBoxRegularPolygonX.Text, labelRegularPolygonX, "X");
+                ProcessInput(xRegularPolygonInput, textBoxRegularPolygonX.Text, labelRegularPolygonX, "X");
         }
     }
 }
